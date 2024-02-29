8858ac300db699be9639878b1129922b75431fe8
Hari Haran.c <harichella005@gmail.com>
2024-02-29 11:18:03 +0530

    wikipedia added and time added

--- main.py
+++ main.py
@@ -2,15 +2,19 @@
 
 from gtts import gTTS
 import speech_recognition as sr
-import pyttsx3
 import os
+import wikipedia
+import datetime
+
+
+hour = int(datetime.datetime.now().hour)
 
 def listen():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         recognizer.adjust_for_ambient_noise(source, duration=1)  # Adjust for ambient noise
-        audio = recognizer.listen(source, timeout=5)  # Timeout after 5 seconds of silence
+        audio = recognizer.listen(source, timeout='')  # Timeout after 5 seconds of silence
 
     try:
         print("Recognizing...")
@@ -24,22 +28,43 @@ def listen():
         print(f"Could not request results from Google Speech Recognition service; {e}")
         return ""
 
+def search_wikipedia(query):
+    try:
+        result = wikipedia.summary(query, sentences=2)
+        speak(f"Wikipedia says: {result}")
+    except wikipedia.exceptions.DisambiguationError as e:
+        speak("Multiple results found. Please be more specific.")
+    except wikipedia.exceptions.PageError as e:
+        speak("No results found. please try again.")
+
 def speak(text):
     tts = gTTS(text=text, lang='en', slow=False)
     tts.save('out.mp3')
     # Play the generated audio file (Linux command, adjust for your system)
-    os.system("play out.mp3")
+    os.system("play -q out.mp3")
 
 if __name__ == "__main__":
-    speak("Hello, I am Jarvis. How can I assist you today?")
+    speak("Hello, How can I assist you today?")
     
     while True:
         command = listen()
         
-        if "stop" in command:
-            speak("Goodbye!")
+        if "stop" in command or "bye" in command:
+            speak("You are welcome, i am here to help you !")
             break
-        elif "your_name" in command:
-            speak("I am Jarvis, your virtual assistant.")
+        
+        elif "good morning" in command or "morning" in command:
+            if hour>= 0 and hour<12:
+                speak("Good morning")
+            elif hour>= 12 and hour<18:
+                speak("good afternoon")
+            else:
+                speak("good evening")
+                
+        elif "your name" in command or "name" in command or "call you" in command:
+            speak("I am Harina, your virtual assistant.")
+        elif "search" in command or "what is" in command or "who is":
+            search_query = command.replace("search", "")
+            search_wikipedia(search_query)
         elif command:
             speak("I'm sorry, I didn't understand that command. Please try again.")
